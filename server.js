4c23212ee9cac4b763258698297f3bdec9452cc3
snigdhavalluri-commits <snigdha.valluri@gmail.com>
2026-06-23 01:16:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("."));
 
 const PROMPTS = {
   general: (food) => `You are a global food ingredient analyst. Analyse this food: "${food}"
